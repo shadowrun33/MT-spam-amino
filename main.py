1a3ef27a8d221ba3d695fdbb1fc68deaa70ce2c6
Shadowrun33 <74319650+shadowrun33@users.noreply.github.com>
2022-01-16 09:03:52 +0300

    changed lib

---
+++ main.py
@@ -0,0 +1,44 @@
+import aminofix
+import pyfiglet
+from colorama import init, Fore, Back, Style
+from threading import Thread
+
+
+init()
+print(Back.BLACK)
+print(Fore.YELLOW)
+
+print(pyfiglet.figlet_format("mt spam", font="big"))
+print("         ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓")
+print("         ┃by shadowrun                   ")
+print("         ┃https://github.com/shadowrun33/")
+print("         ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
+
+email = str(input("Почта/Email > "))
+password = str(input("Пароль/Password > "))
+client = aminofix.Client()
+client.login(email=email, password=password)
+
+print("Успешный вход/Login succeeded")
+
+com = client.sub_clients()
+for name, comId in zip(com.name, com.comId):
+    print(f"{name} - {comId}")
+
+comId = int(input("Id сообщества/Community Id > "))
+
+subclient = aminofix.SubClient(comId = comId, profile = client.profile)
+
+chat = subclient.get_chat_threads(start = 0, size = 100)
+for title, chatId in zip(chat.title, chat.chatId):
+    print(f"{title} - {chatId}")
+chatId = str(input("Id чата/Chat Id > "))
+
+messageType = int(input("Тип сообщений/Message type > "))
+
+message = str(input("Сообщение/Message > "))
+
+while True:
+    threads = [Thread(target = subclient.send_message, args=(chatId, message, messageType)) for x in range(100)]
+    for t in threads:
+        t.start()
